eb212b971715d3fe07fd66eb81abf95c1dcc881c
Roman Gulyako <romangulyako@gmail.com>
2024-12-27 19:49:53 +0300

    Добавил generic-класс DAO, который реализует интерфейс IDAO.
    Его методы могут использоваться для любых Entity.
    Также добавил вспомогающий класс и функциональный интерфейс для сокращения повторяющегося кода.

---
+++ src/main/java/by/itacademy/jd2/dao/DAO.java
@@ -0,0 +1,65 @@
+package by.itacademy.jd2.dao;
+
+import by.itacademy.jd2.dao.api.IDAO;
+import by.itacademy.jd2.utils.ExecutorUtil;
+import by.itacademy.jd2.utils.HibernateUtil;
+
+import javax.persistence.EntityManager;
+import java.io.Serializable;
+import java.util.List;
+
+public abstract class DAO<T> implements IDAO<T> {
+    private final EntityManager em;
+    private final Class<T> clazz;
+
+    public DAO(Class<T> clazz) {
+        this.em = HibernateUtil.getEntityManager();
+        this.clazz = clazz;
+    }
+
+    @Override
+    public T save(T entity) {
+        return ExecutorUtil.executeHibernate(this.em, em -> {
+            em.persist(entity);
+            return entity;
+        });
+    }
+
+    @Override
+    public T update(T entity, Serializable id) {
+        return ExecutorUtil.executeHibernate(this.em, em -> {
+            T updatedEntity;
+            if ((updatedEntity = em.find(this.clazz, id)) != null) {
+                updatedEntity = em.merge(entity);
+            }
+
+            return updatedEntity;
+        });
+    }
+
+    @Override
+    public T get(Serializable id) {
+        return ExecutorUtil.executeHibernate(this.em, em ->
+                em.find(this.clazz, id));
+    }
+
+    @Override
+    public List<T> getAll() {
+        String query = "FROM " + this.clazz.getSimpleName();
+        return ExecutorUtil.executeHibernate(this.em,
+                em -> em.createQuery(query, this.clazz).getResultList());
+    }
+
+    @Override
+    public boolean delete(Serializable id) {
+        return Boolean.TRUE.equals(ExecutorUtil.executeHibernate(this.em, em -> {
+            T t = em.find(this.clazz, id);
+            if (t != null) {
+                em.remove(t);
+                return true;
+            } else {
+                return false;
+            }
+        }));
+    }
+}
---
+++ src/main/java/by/itacademy/jd2/utils/ExecutorUtil.java
@@ -0,0 +1,21 @@
+package by.itacademy.jd2.utils;
+
+import by.itacademy.jd2.utils.api.HibernateExecutor;
+
+import javax.persistence.EntityManager;
+
+public class ExecutorUtil {
+    public static <T> T executeHibernate(EntityManager em,
+                                         HibernateExecutor<T> hibernateExecutor) {
+        try {
+            em.getTransaction().begin();
+            T t = hibernateExecutor.execute(em);
+            em.getTransaction().commit();
+
+            return t;
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            return null;
+        }
+    }
+}
