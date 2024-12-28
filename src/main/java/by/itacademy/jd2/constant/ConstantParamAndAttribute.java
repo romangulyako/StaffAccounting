a0c85d56d4ad2c56d7a80f1912d6936dde883842
Roman Gulyako <romangulyako@gmail.com>
2024-12-28 19:57:18 +0300

    Добавил классы с константами.
    Константы с именами JSP страниц, именами аттрибутов и параметров, а также для имен действий(action в jsp)

---
+++ src/main/java/by/itacademy/jd2/constant/ConstantParamAndAttribute.java
@@ -0,0 +1,17 @@
+package by.itacademy.jd2.constant;
+
+public class ConstantParamAndAttribute {
+    public static String ID = "id";
+    public static String SURNAME = "surname";
+    public static String NAME = "name";
+    public static String PATRONYMIC = "patronymic";
+    public static String BIRTHDAY = "birthday";
+    public static String RESIDENCE_CITY = "residence_city";
+    public static String RESIDENCE_STREET = "residence_street";
+    public static String RESIDENCE_HOUSE = "residence_house";
+    public static String RESIDENCE_APARTMENT = "residence_apartment";
+    public static String PHONE = "phone";
+    public static String LIST_EMPLOYEES = "list_employees";
+    public static String EMPLOYEE = "employee";
+    public static String ERROR = "errorMessage";
+}
