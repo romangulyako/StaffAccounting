4d89ff0baf7aee46f2295a67e498eca43b17456b
Roman Gulyako <romangulyako@gmail.com>
2024-12-28 20:04:02 +0300

    Изменил конвертер
    Добавил метод, который получает EmployeeDTO из объекта HttpServletRequest

--- src/main/java/by/itacademy/jd2/converter/EmployeeConverter.java
+++ src/main/java/by/itacademy/jd2/converter/EmployeeConverter.java
@@ -1,7 +1,13 @@
 package by.itacademy.jd2.converter;
 
+import by.itacademy.jd2.constant.ConstantParamAndAttribute;
 import by.itacademy.jd2.dto.EmployeeDTO;
 import by.itacademy.jd2.repository.EmployeeEntity;
+import by.itacademy.jd2.repository.embedded.Address;
+import by.itacademy.jd2.repository.embedded.PersonData;
+import by.itacademy.jd2.utils.ParseUtil;
+import by.itacademy.jd2.utils.ServletUtil;
+import jakarta.servlet.http.HttpServletRequest;
 
 public class EmployeeConverter {
     public static EmployeeEntity toEntity(EmployeeDTO employeeDTO) {
@@ -19,6 +25,25 @@ public class EmployeeConverter {
                 .personData(employeeEntity.getPersonData())
                 .homeAddress(employeeEntity.getHomeAddress())
                 .phone(employeeEntity.getPhone())
+                .build();
+    }
+
+    public static EmployeeDTO fromHttpRequest(HttpServletRequest req) throws NumberFormatException {
+        return EmployeeDTO.builder()
+                .id(ParseUtil.parseLong(ServletUtil.getParam(req, ConstantParamAndAttribute.ID)))
+                .personData(PersonData.builder()
+                        .surname(ServletUtil.getParam(req, ConstantParamAndAttribute.SURNAME))
+                        .name(ServletUtil.getParam(req, ConstantParamAndAttribute.NAME))
+                        .patronymic(ServletUtil.getParam(req, ConstantParamAndAttribute.PATRONYMIC))
+                        .birthday(ParseUtil.parseDate(ServletUtil.getParam(req, ConstantParamAndAttribute.BIRTHDAY)))
+                        .build())
+                .homeAddress(Address.builder()
+                        .city(ServletUtil.getParam(req, ConstantParamAndAttribute.RESIDENCE_CITY))
+                        .street(ServletUtil.getParam(req, ConstantParamAndAttribute.RESIDENCE_STREET))
+                        .house(ServletUtil.getParam(req, ConstantParamAndAttribute.RESIDENCE_HOUSE))
+                        .apartment(Integer.parseInt(ServletUtil.getParam(req, ConstantParamAndAttribute.RESIDENCE_APARTMENT)))
+                        .build())
+                .phone(ServletUtil.getParam(req, ConstantParamAndAttribute.PHONE))
                 .build();
     }
 }
