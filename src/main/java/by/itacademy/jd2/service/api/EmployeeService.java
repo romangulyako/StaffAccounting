6fb5101cadf239d8e1ef86538b3576eaf37b9f25
Roman Gulyako <romangulyako@gmail.com>
2025-01-01 21:56:49 +0300

    Изменил  Service
    Добавил методы для работы с паспортом

--- src/main/java/by/itacademy/jd2/service/api/EmployeeService.java
+++ src/main/java/by/itacademy/jd2/service/api/EmployeeService.java
@@ -1,6 +1,7 @@
 package by.itacademy.jd2.service.api;
 
 import by.itacademy.jd2.dto.EmployeeDTO;
+import by.itacademy.jd2.dto.PassportDTO;
 
 import java.io.Serializable;
 import java.util.List;
@@ -11,4 +12,8 @@ public interface EmployeeService extends Service {
     void deleteEmployee(Serializable id);
     EmployeeDTO getEmployee(Serializable id);
     List<EmployeeDTO> getAllEmployees();
+    void addPassport(PassportDTO passportDTO, EmployeeDTO employeeDTO);
+    void updatePassport(PassportDTO passportDTO, EmployeeDTO employeeDTO);
+    void deletePassport(Serializable id);
+    PassportDTO getPassport(Serializable id);
 }
