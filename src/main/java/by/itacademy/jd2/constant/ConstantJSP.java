2b91c4525aaf759cf1f2f23eda9a6fcd1a016653
Roman Gulyako <romangulyako@gmail.com>
2025-01-04 17:24:52 +0300

    Добавил константы в классы констант

--- src/main/java/by/itacademy/jd2/constant/ConstantJSP.java
+++ src/main/java/by/itacademy/jd2/constant/ConstantJSP.java
@@ -15,5 +15,8 @@ public class ConstantJSP {
     public static final String MARITAL_STATUSES_PAGE = "/marital_statuses/marital_statuses.jsp";
     public static final String ADD_MARITAL_STATUS_PAGE = "/marital_statuses/add_marital_status.jsp";
     public static final String UPDATE_MARITAL_STATUS_PAGE = "/marital_statuses/update_marital_status.jsp";
+    public static final String EDUCATION_PAGE = "/education/education.jsp";
+    public static final String ADD_EDUCATION_PAGE = "/education/add_education.jsp";
+    public static final String UPDATE_EDUCATION_PAGE = "/education/update_education.jsp";
 }
 
