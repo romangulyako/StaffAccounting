62f4a2e77ed3b41bfeef3802101cdf4fa48d959d
Roman Gulyako <romangulyako@gmail.com>
2025-01-05 02:05:35 +0300

    Добавил сервис для departments

---
+++ src/main/java/by/itacademy/jd2/service/api/DepartmentService.java
@@ -0,0 +1,14 @@
+package by.itacademy.jd2.service.api;
+
+import by.itacademy.jd2.dto.DepartmentDTO;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface DepartmentService extends Service {
+    void addDepartment(DepartmentDTO departmentDTO);
+    void updateDepartment(DepartmentDTO departmentDTO);
+    void deleteDepartment(Serializable id);
+    DepartmentDTO getDepartment(Serializable id);
+    List<DepartmentDTO> getAllDepartments();
+}
