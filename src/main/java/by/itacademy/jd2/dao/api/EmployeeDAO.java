f5a526f9a011d4facbedea20eefbf5ea2ec6c084
Roman Gulyako <romangulyako@gmail.com>
2025-01-11 13:57:57 +0300

    Добавил отдельные методы для получения действующих сотрудников и уволенных

--- src/main/java/by/itacademy/jd2/dao/api/EmployeeDAO.java
+++ src/main/java/by/itacademy/jd2/dao/api/EmployeeDAO.java
@@ -2,5 +2,9 @@ package by.itacademy.jd2.dao.api;
 
 import by.itacademy.jd2.entity.EmployeeEntity;
 
+import java.util.List;
+
 public interface EmployeeDAO extends IDAO<EmployeeEntity> {
+    List<EmployeeEntity> getAllCurrentEmployees();
+    List<EmployeeEntity> getAllFiredEmployees();
 }
--- src/main/java/by/itacademy/jd2/dao/impl/EmployeeDaoImpl.java
+++ src/main/java/by/itacademy/jd2/dao/impl/EmployeeDaoImpl.java
@@ -3,9 +3,36 @@ package by.itacademy.jd2.dao.impl;
 import by.itacademy.jd2.dao.DAO;
 import by.itacademy.jd2.dao.api.EmployeeDAO;
 import by.itacademy.jd2.entity.EmployeeEntity;
+import by.itacademy.jd2.utils.ExecutorUtil;
+
+import javax.persistence.Query;
+import java.util.List;
 
 public class EmployeeDaoImpl extends DAO<EmployeeEntity> implements EmployeeDAO {
+    private static final String GET_EMPLOYEES_QUERY = "SELECT e " +
+            "FROM EmployeeEntity e WHERE isFired =: isFired";
+    private static final String IS_FIRED_PARAMETER = "isFired";
+
     public EmployeeDaoImpl() {
         super(EmployeeEntity.class);
+    }
+
+    @Override
+    public List<EmployeeEntity> getAllCurrentEmployees() {
+        return getEmployeesByFired(false);
+    }
+
+    @Override
+    public List<EmployeeEntity> getAllFiredEmployees() {
+        return getEmployeesByFired(true);
+    }
+
+    private List<EmployeeEntity> getEmployeesByFired(boolean isFired) {
+        return ExecutorUtil.executeHibernate(super.getEntityManager(),
+                em -> {
+                    Query query = em.createQuery(GET_EMPLOYEES_QUERY);
+                    query.setParameter(IS_FIRED_PARAMETER, isFired);
+                    return (List<EmployeeEntity>) query.getResultList();
+                });
     }
 }
