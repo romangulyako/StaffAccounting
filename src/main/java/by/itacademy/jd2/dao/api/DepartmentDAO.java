b56a788c849e4068e7e588e78207fe7c2c055722
Roman Gulyako <romangulyako@gmail.com>
2025-01-12 21:47:44 +0300

    Добавил пагинацию на страницу отделов

--- src/main/java/by/itacademy/jd2/dao/api/DepartmentDAO.java
+++ src/main/java/by/itacademy/jd2/dao/api/DepartmentDAO.java
@@ -2,5 +2,9 @@ package by.itacademy.jd2.dao.api;
 
 import by.itacademy.jd2.entity.DepartmentEntity;
 
+import java.util.List;
+
 public interface DepartmentDAO extends IDAO<DepartmentEntity> {
+    List<DepartmentEntity> getDepartmentsByPage(Integer pageSize, Integer pageNumber);
+    Long getDepartmentsCount();
 }
