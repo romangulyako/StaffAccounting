4503823c7e5d1caa89224c57f8c75f3300cbb27e
Roman Gulyako <romangulyako@gmail.com>
2025-01-15 16:35:18 +0300

    Добавил условие по полю isActual в запросы DepartmentDAO

--- src/main/java/by/itacademy/jd2/dao/impl/DepartmentDaoImpl.java
+++ src/main/java/by/itacademy/jd2/dao/impl/DepartmentDaoImpl.java
@@ -9,19 +9,23 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class DepartmentDaoImpl extends DAO<DepartmentEntity> implements DepartmentDAO {
-    private static final String GET_ALL_DEPARTMENTS_QUERY = "select d from DepartmentEntity d";
-    private static final String GET_DEPARTMENTS_COUNT_QUERY = "SELECT COUNT(*) FROM DepartmentEntity";
+    private static final String IS_ACTUAL_PARAMETER = "isActual";
+    private static final String GET_ALL_DEPARTMENTS_QUERY =
+            "select d from DepartmentEntity d where d.isActual =: isActual";
+    private static final String GET_DEPARTMENTS_COUNT_QUERY =
+            "SELECT COUNT(d) FROM DepartmentEntity d WHERE d.isActual =: isActual";
 
     public DepartmentDaoImpl() {
         super(DepartmentEntity.class);
     }
 
     @Override
-    public List<DepartmentEntity> getDepartmentsByPage(Integer pageSize, Integer pageNumber) {
+    public List<DepartmentEntity> getDepartmentsByActualAndPage(Boolean isActual, Integer pageSize, Integer pageNumber) {
         return ExecutorUtil.executeHibernate(super.getEntityManager(),
                 em -> {
                     TypedQuery<DepartmentEntity> query = em.createQuery(GET_ALL_DEPARTMENTS_QUERY,
                             DepartmentEntity.class);
+                    query.setParameter(IS_ACTUAL_PARAMETER, isActual);
                     query.setFirstResult((pageNumber - 1) * pageSize);
                     query.setMaxResults(pageSize);
                     return query.getResultList();
@@ -29,10 +33,11 @@ public class DepartmentDaoImpl extends DAO<DepartmentEntity> implements Departme
     }
 
     @Override
-    public Long getDepartmentsCount() {
+    public Long getDepartmentsCountByActual(Boolean isActual) {
         return ExecutorUtil.executeHibernate(super.getEntityManager(),
                 em -> {
                     TypedQuery<Long> query = em.createQuery(GET_DEPARTMENTS_COUNT_QUERY, Long.class);
+                    query.setParameter(IS_ACTUAL_PARAMETER, isActual);
                     return query.getSingleResult();
                 });
     }
